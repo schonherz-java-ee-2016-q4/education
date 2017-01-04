d8b2d41bb42f376b7d79a946c32e97b68b85a366
attilagyongyosi <attilagyongyosi0@gmail.com>
2017-01-04 17:16:43 +0100

    More Spring example documentation.

--- 10_Spring/example/springmvcexample/src/main/java/hu/schonherz/training/springmvcexample/web/controllers/ParticipantController.java
+++ 10_Spring/example/springmvcexample/src/main/java/hu/schonherz/training/springmvcexample/web/controllers/ParticipantController.java
@@ -15,14 +15,34 @@ public class ParticipantController {
     @Autowired
     private ParticipantService participantService;
 
+    /**
+     * Handles calls to /participant/.
+     * Will attach a model attribute named 'model' onto the request scope before
+     * forwarding to index.jsp.
+     *
+     * This 'model' will be the backing bean of the form on the index page
+     * (note the modelAttribute HTML attribute on the form tag!)
+     * The model attribute itself will be an empty {@link ParticipantModel} instance.
+     *
+     * By filling the form, you will actually set the fields of this empty {@link ParticipantModel}.
+     */
     @GetMapping(path = "/")
     public ModelAndView newParticipant() {
-        return new ModelAndView("index", "model", new ParticipantModel());
+        return new ModelAndView(
+                "index", // view name
+                "model", // model name
+                new ParticipantModel()); // actual model instance
     }
 
     // @PostMapping is a Spring MVC 4 shorthand for @RequestMapping with method = RequestMethod.POST
+
+    /**
+     * Handles POST requests to /participants/save.
+     * Saves the form's backing bean (the ParticipantModel supplied by the previous method)
+     * to the database and forwards to the success.jsp
+     */
     @RequestMapping(path = "/save", method = RequestMethod.POST)
-    public String saveParticipant(final @ModelAttribute ParticipantModel model) {
+    public String saveParticipant(final @ModelAttribute ParticipantModel model /* form backing bean */) {
         participantService.save(toParticipant(model));
         return "success";
     }
