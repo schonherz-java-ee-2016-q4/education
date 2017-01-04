17fa28a945756e8083278d15a444378b97c2185d
attilagyongyosi <attilagyongyosi0@gmail.com>
2017-01-04 12:52:09 +0100

    Renaming Security folder, adding JPA presentation and example code.

---
+++ 13_JPA/example/blog/blog-core/src/main/java/hu/schonhertz/training/blog/entity/Blog.java
@@ -0,0 +1,78 @@
+package hu.schonhertz.training.blog.entity;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+/**
+ * The Class User.
+ */
+@Entity
+@Table(name = "Blog")
+public class Blog extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    private User creator;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "blog_id")
+    private Set<Comment> comments;
+
+    @Basic
+    private String title;
+
+    @Lob
+    private String text;
+
+    /**
+     * @return the creator
+     */
+    public User getCreator() {
+        return creator;
+    }
+
+    /**
+     * @param creator the creator to set
+     */
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+
+    /**
+     * @param text the text to set
+     */
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+}
---
+++ 13_JPA/example/blog/blog-core/src/main/java/hu/schonhertz/training/blog/entity/Comment.java
@@ -0,0 +1,38 @@
+package hu.schonhertz.training.blog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * The Class User.
+ */
+@Entity
+@Table(name = "Comment")
+public class Comment extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Lob
+	private String comment;
+
+	@ManyToOne
+	private Blog blog;
+
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+
+	/**
+	 * @param comment
+	 *            the comment to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+}
