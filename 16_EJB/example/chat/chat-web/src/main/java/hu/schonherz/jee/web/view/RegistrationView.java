3f687645c07ca21d4e93f52c5d8e9a759931a912
Zelei Attila <zelei.attila@neuron.hu>
2017-01-14 16:31:13 +0100

    next step

---
+++ 16_EJB/example/chat/chat-web/src/main/java/hu/schonherz/jee/web/view/RegistrationView.java
@@ -0,0 +1,53 @@
+package hu.schonherz.jee.web.view;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import hu.schonherz.jee.service.client.api.service.user.UserServiceRemote;
+import hu.schonherz.jee.service.client.api.vo.UserVo;
+
+@ManagedBean(name = "registrationView")
+@ViewScoped
+public class RegistrationView {
+
+	@EJB
+	private UserServiceRemote serviceRemote;
+
+	private UserVo userVo;
+
+	@PostConstruct
+	public void init() {
+		userVo = new UserVo();
+	}
+
+	public void registration() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			serviceRemote.registrationUser(userVo);
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successsssss!", "Registration!"));
+		} catch (Exception e) {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error!", "Registration!"));
+
+		}
+	}
+
+	public UserVo getUserVo() {
+		return userVo;
+	}
+
+	public void setUserVo(UserVo userVo) {
+		this.userVo = userVo;
+	}
+
+	public UserServiceRemote getServiceRemote() {
+		return serviceRemote;
+	}
+
+	public void setServiceRemote(UserServiceRemote serviceRemote) {
+		this.serviceRemote = serviceRemote;
+	}
+}
